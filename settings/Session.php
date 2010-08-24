3038beba410adab50f66a4e50eba5258c7f1b0de
A.J. Cates <aj@ajcates.com>
2010-08-23 19:31:08 -0700

    changed the session hash

--- settings/Session.php
+++ settings/Session.php
@@ -3,7 +3,7 @@ SweetFramework::getClass('lib', 'Config')->setAll('Session', array(
 	'timeout' => 31536000,
 	'hashFunction' => 'sha512',
 	'cookieSecret' => 'h07rsouY43hSNpNAVvcEKDrasdfasdsdfasedefeeeasx5RjTfrthw49BC6xeGNvw2nI55z1RH',
-	'cookieName' => 'crad-token',
+	'cookieName' => 'sweet-hash',
 	'use' => array($_SERVER['REMOTE_ADDR']),
 	'sslCookies' => false,
 	'tableName' => 'Sessions',
