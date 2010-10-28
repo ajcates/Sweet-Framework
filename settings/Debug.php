120fc40bd463115ca5457e3385b5999a71797f09
A.J. Cates <aj@ajcates.com>
2010-10-27 17:13:56 -0700

    fixed a syntxt error

--- settings/Debug.php
+++ settings/Debug.php
@@ -1,17 +1,4 @@
 <?
-SweetFramework::getClass('lib', 'Config')->setAll('Debug', array(
-	'debug' => true,
-	'warnings' => true,
-	'logfile' => LOC . '/sweet-framework/logs/main.log',
-	'growl' => array(
-		'host' => 'localhost',
-		'password' => 'aldo20'
-	)
-));
-
-
-
-<?php
 switch ($_SERVER['SERVER_NAME']) {
     case 'ajcates-macbook.local':
  		SweetFramework::getClass('lib', 'Config')->setAll('Debug', array(
