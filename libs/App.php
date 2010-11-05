11b55735b549975db64cb4e8e58712cb2d10cf35
A.J. Cates <aj@ajcates.com>
2010-11-04 22:15:49 -0700

    updated App amazingly enough

--- libs/App.php
+++ libs/App.php
@@ -8,21 +8,21 @@ class App {
 	
 	function lib($lib) {
 		if(is_array($lib)) {
-			return f_last(array_map(f_callable(array($this, 'lib')), $lib));
+			return f_last(array_map(array($this, 'lib'), $lib));
 		}
 		return $this->libs->{SweetFramework::className($lib)} = SweetFramework::getClass('lib', $lib);
 	}
 	
 	function model($model) {
 		if(is_array($model)) {
-			return f_last(array_map(f_callable(array($this, 'model')), $model));
+			return f_last(array_map(array($this, 'model'), $model));
 		}
 		return $this->models->{SweetFramework::className($model)} = SweetFramework::getClass('model', $model);
 	}
 	
 	function helper($helper) {
 		if(is_array($helper)) {
-			return f_last(array_map(f_callable(array($this, 'helper')), $helper));
+			return f_last(array_map(array($this, 'helper'), $helper));
 		}
 		return SweetFramework::loadFileType('helper', $helper);
 	}
