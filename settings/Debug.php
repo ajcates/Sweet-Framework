92d85969e16e9a620be81597a86f74155ce98a5c
ajcates <aj@ajcates.com>
2011-03-18 11:21:26 -0700

    fixed Debug settings

--- settings/Debug.php
+++ settings/Debug.php
@@ -1,31 +1,4 @@
 <?
-<<<<<<< HEAD
-//Development:
-SweetFramework::getClass('lib', 'Config')->setAll('Debug', array(
-	'debug' => true,
-	'warnings' => true,
-	'logfile' => LOC . '/sweet-framework/logs/main.log',
-	'growl' => array(
-		'host' => 'localhost',
-		'password' => 'aldo20'
-	)
-));
-//Production:
-/*
-SweetFramework::getClass('lib', 'Config')->setAll('Debug', array(
-	'debug' => false,
-	'warnings' => false,
-	'logfile' => LOC . '/sweet-framework/logs/main.log',
-	'growl' => array(
-		'host' => 'localhost',
-		'password' => 'aldo20'
-	)
-));
-break;
-*/
-=======
-
-
 if(stristr($_SERVER['HTTP_HOST'], 'local') !== false || stristr($_SERVER['HTTP_HOST'], 'dev') !== false) {
 	SweetFramework::getClass('lib', 'Config')->setAll('Debug', array(
 		'debug' => true,
@@ -47,4 +20,3 @@ if(stristr($_SERVER['HTTP_HOST'], 'local') !== false || stristr($_SERVER['HTTP_H
 		)
 	));
 }
->>>>>>> a25dd950c021cb240f26ec7db4b84ae237c973d5
