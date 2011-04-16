9f14fc9a3ebb88945fc8ed93680988e7be09c8e7
AJ Cates <ajcates@donkey.(none)>
2011-04-15 17:39:00 -0700

    added in new settings

--- settings/Debug.php
+++ settings/Debug.php
@@ -1,24 +1,24 @@
 <?
-//Development:
-SweetFramework::getClass('lib', 'Config')->setAll('Debug', array(
-	'debug' => true,
-	'warnings' => true,
-	'logfile' => LOC . '/sweet-framework/logs/main.log',
-	'growl' => array(
-		'host' => 'localhost',
-		'password' => 'aldo20'
-	)
-));
-//Production:
-/*
-SweetFramework::getClass('lib', 'Config')->setAll('Debug', array(
-	'debug' => false,
-	'warnings' => false,
-	'logfile' => LOC . '/sweet-framework/logs/main.log',
-	'growl' => array(
-		'host' => 'localhost',
-		'password' => 'aldo20'
-	)
-));
-break;
-*/
+
+
+if(stristr($_SERVER['HTTP_HOST'], 'local') !== false || stristr($_SERVER['HTTP_HOST'], 'dev') !== false) {
+	SweetFramework::getClass('lib', 'Config')->setAll('Debug', array(
+		'debug' => true,
+		'warnings' => true,
+		'logfile' => LOC . '/sweet-framework/logs/main.log',
+		'growl' => array(
+			'host' => 'localhost',
+			'password' => 'aldo20'
+		)
+	));
+} else {
+	SweetFramework::getClass('lib', 'Config')->setAll('Debug', array(
+		'debug' => false,
+		'warnings' => false,
+		'logfile' => LOC . '/sweet-framework/logs/main.log',
+		'growl' => array(
+			'host' => 'localhost',
+			'password' => 'aldo20'
+		)
+	));
+}
