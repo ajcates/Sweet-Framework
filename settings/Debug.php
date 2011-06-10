67e80628c31b948473cd7c659334376ec77ecf91
ajcates <aj@ajcates.com>
2011-06-09 17:19:47 -0700

    made my debug settings a little cleaner

--- settings/Debug.php
+++ settings/Debug.php
@@ -1,22 +1,25 @@
-<?
-if(!isset($_SERVER['HTTP_HOST']) || (stristr($_SERVER['HTTP_HOST'], 'local') !== false || stristr($_SERVER['HTTP_HOST'], 'dev') !== false)) {
-	SweetFramework::getClass('lib', 'Config')->setAll('Debug', array(
-		'debug' => true,
-		'warnings' => true,
-		'logfile' => LOC . '/sweet-framework/logs/main.log',
-		'growl' => array(
-			'host' => 'localhost',
-			'password' => 'aldo20'
-		)
-	));
-} else {
-	SweetFramework::getClass('lib', 'Config')->setAll('Debug', array(
-		'debug' => false,
-		'warnings' => false,
-		'logfile' => LOC . '/sweet-framework/logs/main.log',
-		'growl' => array(
-			'host' => 'localhost',
-			'password' => 'aldo20'
-		)
-	));
+<?php
+switch(trim(`hostname`)) {
+    case 'ajcates-macbook.local':
+        SweetFramework::getClass('lib', 'Config')->setAll('Debug', array(
+            'debug' => true,
+            'warnings' => true,
+            'logfile' => LOC . '/sweet-framework/logs/main.log',
+            'growl' => array(
+                'host' => 'localhost',
+                'password' => 'aldo20'
+            )
+        ));
+		break;
+	default:
+        SweetFramework::getClass('lib', 'Config')->setAll('Debug', array(
+            'debug' => false,
+            'warnings' => false,
+            'logfile' => LOC . '/sweet-framework/logs/main.log',
+            'growl' => array(
+                'host' => 'localhost',
+                'password' => 'aldo20'
+            )
+        ));
+		break;
 }
