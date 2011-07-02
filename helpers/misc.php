a79d4b75ce8328c6c32c1b71bb47a410849358b8
ajcates <aj@ajcates.com>
2011-07-01 17:52:40 -0700

    added in a array_merge_recusive_simple function

--- helpers/misc.php
+++ helpers/misc.php
@@ -205,6 +205,37 @@ function stacktrace() {
 	);
 }
 
+function array_merge_recursive_simple() {
+    //Orignally written by walf
+    //http://www.php.net/manual/en/function.array-merge-recursive.php#104145
+
+    if (func_num_args() < 2) {
+        trigger_error(__FUNCTION__ .' needs two or more array arguments', E_USER_WARNING);
+        return;
+    }
+    $arrays = func_get_args();
+    $merged = array();
+    while ($arrays) {
+        $array = array_shift($arrays);
+        if (!is_array($array)) {
+            trigger_error(__FUNCTION__ .' encountered a non array argument', E_USER_WARNING);
+            return;
+        }
+        if (!$array)
+            continue;
+        foreach ($array as $key => $value)
+            if (is_string($key))
+                if (is_array($value) && array_key_exists($key, $merged) && is_array($merged[$key]))
+                    $merged[$key] = call_user_func(__FUNCTION__, $merged[$key], $value);
+                else
+                    $merged[$key] = $value;
+            else
+                $merged[] = $value;
+    }
+    return $merged;
+}
+
+
 function objToArray($obj) {
 	$dataArray = array();
 	foreach($obj as $k => $v) {
