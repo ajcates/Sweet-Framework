e429ca83a356343a4427f046b409846a544d3f79
ajcates <aj@ajcates.com>
2011-07-18 08:44:44 -0700

    made it display the sql on fail

--- libs/databases/drivers/My_SQL.php
+++ libs/databases/drivers/My_SQL.php
@@ -83,6 +83,7 @@ class My_SQL {
 		$this->prepared = false;
 		
 		if(!$this->result) {
+            D::show($sql);
 			D::stack();
 			D::log(mysql_error($this->connection), 'SQL Errors');
 			return false;
