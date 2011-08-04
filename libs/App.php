f1eb70d2a2d3ebf021f69905afba107d08605661
ajcates <aj@ajcates.com>
2011-08-03 19:54:08 -0700

    comments

--- libs/App.php
+++ libs/App.php
@@ -4,8 +4,6 @@ class App {
 	public $libs;
 	public $models;
 
-	function __construct() {}
-	
 	function lib($lib) {
 		if(is_array($lib)) {
 			return f_last(array_map(array($this, 'lib'), $lib));
@@ -27,3 +25,18 @@ class App {
 		return SweetFramework::loadFileType('helper', $helper);
 	}
 }
+
+/*
+    See PHP doesn't always have to be ugly. This part of the framework is so nice
+    I don't even want to put comments in it.
+
+    Pretty much every fricking thing in the framework extends this. So I try and
+    keep things as lite as possible.
+
+    See the trick is the app class only uses static methods of the SweetFramework,
+    so I can use right away.
+
+    Another trick is the check to see if the argument is an array, and if it is, it
+    will map the array to the currently called function. This lets you load up a
+    bunch of models/libs/helpers with one call.
+*/
